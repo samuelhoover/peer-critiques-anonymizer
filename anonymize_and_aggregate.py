bbddb1516662f891ee5ce49f3fa6b07995bdd5ad
samuelhoover <hooversamuelc@gmail.com>
2023-09-07 15:10:15 -0400

    Moved functions into class, modified save path

--- anonymize_and_aggregate.py
+++ anonymize_and_aggregate.py
@@ -64,68 +64,75 @@ import argparse
 import openpyxl
 
 
-def get_path():
-    """
-    parse the argument from the command line.
+class Anonymizer():
+    def __init__(self, path=''):
+        self.path = path
 
-    returns:
-      - path ([string]): path to directory with submissions
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-p', '--path', action='store',
-        help='path to directory with peer reviews'
-    )
-    args = parser.parse_args()
-    path = args.path
+    def get_path(self):
+        """
+        parse the argument from the command line.
 
-    return path
+        returns:
+        - path ([string]): path to directory with submissions
+        """
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '-p', '--path', action='store',
+            help='path to directory with peer reviews'
+        )
+
+        return parser.parse_args().path
 
 
-def anonymize_reviews(path):
-    """
-    Remove the name of reviewer and save graded files in .xlxs format.
+    def anonymize_reviews(self, path):
+        """
+        Remove the name of reviewer and save graded files in .xlxs format.
 
-    Arguments:
-      - path ([string]): path to directory with submissions
-    """
-    save_path = os.path.join(path, 'graded_copies')
-    if os.path.exists(save_path):  # check if graded_copies already exists
-        print('\ngraded-copies directory already exists, removing and starting fresh.')
-        shutil.rmtree(save_path)  # if exists, delete
+        Arguments:
+        - path ([string]): path to directory with submissions
+        """
+        save_path = os.path.join(path, 'graded_copies')
+        if os.path.exists(save_path):  # check if graded_copies already exists
+            print('\ngraded-copies directory already exists, removing and starting fresh.')
+            shutil.rmtree(save_path)  # if exists, delete
 
-    os.mkdir(save_path)  # create directory for graded copies
+        os.mkdir(save_path)  # create directory for graded copies
 
-    count = 1
-    reviewers = [x for x in os.listdir(path) if x not in ['graded_copies', '.DS_Store']]
-    for rev in reviewers:
-        for xls in os.listdir(os.path.join(path, rev)):  # reviews
+        count = 1
+        reviewers = [x for x in os.listdir(path) if x not in ['graded_copies', '.DS_Store']]
+        for rev in reviewers:
+            for xls in os.listdir(os.path.join(path, rev)):  # reviews
 
-            xls = os.path.join(path, rev, xls)  # create path to spreadsheet
-            wb_obj = openpyxl.load_workbook(xls, data_only=True)  # load spreadsheet
-            wb_obj.worksheets[0]['D6'] = ''  # remove reviewer name
+                xls = os.path.join(path, rev, xls)  # create path to spreadsheet
+                wb_obj = openpyxl.load_workbook(xls, data_only=True)  # load spreadsheet
+                wb_obj.worksheets[0]['D6'] = ''  # remove reviewer name
 
-            sheet_obj = wb_obj.active
-            rows = [14, 12, 10]  # indices for experiment, speaker, group
+                sheet_obj = wb_obj.active
 
-            # save and rename graded sheet
-            nem = f'{xls.split(".")[0]}graded.xlsx'
-            for i in rows:
-                cell_obj = sheet_obj.cell(row=i, column=4)
-                nem = f'{cell_obj.value}_{nem}'
+                # save and rename graded sheet
+                nem = f'{xls.split(".")[0]}graded.xlsx'
+                for i in [14, 10]:  # indices for experiment, speaker
+                    cell_obj = sheet_obj.cell(row=i, column=4)
+                    nem = f'{cell_obj.value}_{nem}'
 
-            nem = f'{count}_{nem}'.replace(' ', '')  # anonymized sheet file name
-            wb_obj.save(filename=os.path.join(save_path, nem))
+                if set(nem.split('_')[:2]) != {'None'}:  # if not empty sheet
+                    nem = f'{count:02}_{nem}'.replace(' ', '-')  # anonymized sheet file name
+                    wb_obj.save(filename=os.path.join(save_path, nem.lower()))
+                    
+                    count += 1
 
-            count += 1
+    def aggregate(self, path):
+        print(set([x.lower().split('_')[1] for x in os.listdir(os.path.join(path, 'graded_copies'))]))
 
 
 def main():
     """
     Get path and then anonymize and aggregate peer critiques.
     """
-    path = get_path()
-    anonymize_reviews(path)
+    anonymizer = Anonymizer()
+    path = anonymizer.get_path()
+    anonymizer.anonymize_reviews(path)
+    anonymizer.aggregate(path)
 
 
 if __name__ == '__main__':

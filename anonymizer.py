983365c1ff906cb0bea791780629f386a1ee4db5
samuelhoover <hooversamuelc@gmail.com>
2023-09-12 16:08:11 -0400

    Anonymized copies now saved in folders for each speaker

--- anonymizer.py
+++ anonymizer.py
@@ -10,7 +10,7 @@ This module anonymizes and aggregates the peer critiques in the given directory.
 Written by Akash Jain for CHE-402-SPRING-2020,
 modified by Sam Hoover for CHE-401-FALL-2022.
 
-Run code with `python3 anonymize_and_aggregate.py --path <path to folder with submissions>`.
+Run code with `python3 anonymizer.py --path <path to folder with submissions>`.
 
 ************
 *** DEMO ***
@@ -28,33 +28,37 @@ Using the below file structure as an example,
 |   |   +---section-01
 |   |   |   +---round-01
 |   |   |   |   +---progress-reports
-|   |   |   |   |   +---reviewer_01
-|   |   |   |   |   |       review_01.xlsx
-|   |   |   |   |   |       review_02.xlsx
-|   |   |   |   |   |       review_03.xlsx
-|   |   |   |   |   +---reviewer_02
-|   |   |   |   |   |       review_01.xlsx
-|   |   |   |   |   |       review_02.xlsx
-|   |   |   |   |   |       review_03.xlsx
+|   |   |   |   |   +---reviewers
+|   |   |   |   |   |   +---reviewer_01
+|   |   |   |   |   |   |       review_01.xlsx
+|   |   |   |   |   |   |       review_02.xlsx
+|   |   |   |   |   |   |       review_03.xlsx
+|   |   |   |   |   +---speakers
+|   |   |   |   |   |   +---speaker_01
+|   |   |   |   |   |   |       anonymized_review_01.xlsx
+|   |   |   |   |   |   |       anonymized_review_02.xlsx
+|   |   |   |   |   |   |       anonymized_review_03.xlsx
 |   :   :   :   :   :
 |   :   :   :   :   :
 |   |   |   |   +---proposals
-|   |   |   |   |   +---reviewer_01
-|   |   |   |   |   |       review_01.xlsx
-|   |   |   |   |   |       review_02.xlsx
-|   |   |   |   |   |       review_03.xlsx
-|   |   |   |   |   +---reviewer_02
-|   |   |   |   |   |       review_01.xlsx
-|   |   |   |   |   |       review_02.xlsx
-|   |   |   |   |   |       review_03.xlsx
+|   |   |   |   |   +---reviewers
+|   |   |   |   |   |   +---reviewer_01
+|   |   |   |   |   |   |       review_01.xlsx
+|   |   |   |   |   |   |       review_02.xlsx
+|   |   |   |   |   |   |       review_03.xlsx
+|   |   |   |   |   +---speakers
+|   |   |   |   |   |   +---speaker_01
+|   |   |   |   |   |   |       anonymized_review_01.xlsx
+|   |   |   |   |   |   |       anonymized_review_02.xlsx
+|   |   |   |   |   |   |       anonymized_review_03.xlsx
 :   :   :   :   :   :
 :   :   :   :   :   :
 =======================================================================
 
-run `python3 anonymize_and_aggregate.py --path ../peer-evaluations/section-01/round-01/proposals`
+run `python3 anonymizer.py --path ../peer-evaluations/section-01/round-01/proposals`
 from the `anonymize-and-aggregate` directory to anonymize and aggregate the Section 1
 proposal presentation peer critiques. All of the anonymized copies will be stored in the
-../peer-evaluations/section-01/round-01/proposals/graded-copies` directory.
+../peer-evaluations/section-01/round-01/proposals/speakers` directory.
 
 Make sure to install the required packages. Run `pip install -r requirements.txt` if unsure.
 """
@@ -91,16 +95,17 @@ class Anonymizer():
         Arguments:
         - path ([string]): path to directory with submissions
         """
-        save_path = os.path.join(path, 'graded_copies')
+        speakers = {}
+        save_path = os.path.join(path, '../speakers')
         if os.path.exists(save_path):  # check if graded_copies already exists
-            print('\ngraded-copies directory already exists, removing and starting fresh.')
+            print('\nspeakers directory already exists, removing and starting fresh.\n')
             shutil.rmtree(save_path)  # if exists, delete
 
-        os.mkdir(save_path)  # create directory for graded copies
+        os.mkdir(save_path)  # create directory for anonymized copies to return to speaker
 
         count = 1
-        reviewers = [x for x in os.listdir(path) if x not in ['graded_copies', '.DS_Store']]
-        for rev in reviewers:
+        reviewers = [x for x in os.listdir(path) if x.endswith('_assignsubmission_file_')]
+        for rev in reviewers:  # reviewers
             for xls in os.listdir(os.path.join(path, rev)):  # reviews
 
                 xls = os.path.join(path, rev, xls)  # create path to spreadsheet
@@ -108,19 +113,31 @@ class Anonymizer():
                 wb_obj.worksheets[0]['D6'] = ''  # remove reviewer name
 
                 sheet_obj = wb_obj.active
-                exp = sheet_obj.cell(row=14, column=4).value
-                speaker = sheet_obj.cell(row=10, column=4).value
-                if speaker is None:  # if empty sheet, skip
-                    print(f'An exception occured in review #{count:02}')
+
+                # extract, transform to lower case, and replace space with hyphen if cells not empty, else skip
+                try:
+                    sp = sheet_obj.cell(row=10, column=4).value.lower().replace(' ', '-')
+                    exp = sheet_obj.cell(row=14, column=4).value.lower().replace(' ', '-')
+                except:  # else, skip
+                    print(f'An exception occured in review #{count:02} -> {rev}.')
                     break
 
-                nem = f'{count:02}_{speaker}_{exp}_graded.xlsx'.lower().replace(' ', '-')
-                wb_obj.save(filename=os.path.join(save_path, nem))
+                nem = f'{count:02}_{sp}_{exp}_anonymized_copy.xlsx'  # anonymized copy file name
+
+                # create folder for speaker if does not exist, save to folder
+                if sp in speakers:
+                    speakers[sp].append(nem)
+                else:
+                    speakers[sp] = [nem]
+                
+                # check if folder exists for speaker, if not create
+                if not os.path.exists(os.path.join(save_path, sp)):
+                    os.mkdir(os.path.join(save_path, sp))
+
+                # save anonymized copy to folder for speaker
+                wb_obj.save(filename=os.path.join(save_path, sp, nem))
                 
                 count += 1
-
-    def aggregate(self, path):
-        print(set([x.lower().split('_')[1] for x in os.listdir(os.path.join(path, 'graded_copies'))]))
 
 
 def main():
@@ -130,7 +147,6 @@ def main():
     anonymizer = Anonymizer()
     path = anonymizer.get_path()
     anonymizer.anonymize_reviews(path)
-    anonymizer.aggregate(path)
 
 
 if __name__ == '__main__':
